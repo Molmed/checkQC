bd58c8080b5d5003abda24238a49f7966ef42b45
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2017-07-03 09:44:36 +0200

    Adding test data

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,12 @@
 
 import json
+import os
 
 
 def get_stats_json():
-    with open("Stats.json") as f:
+    with open(os.path.join(
+            os.path.dirname(__file__),
+            "resources",
+            "150418_SN7001335_0149_AH32CYBCXX",
+            "Stats.json")) as f:
         return json.load(f)
