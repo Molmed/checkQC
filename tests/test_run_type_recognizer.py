016abf11becefb0a4b14b1a6f0f3ace76aefc632
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2017-08-08 16:30:46 +0200

    Adding actual tests (that were missing last time)

--- tests/test_run_type_recognizer.py
+++ tests/test_run_type_recognizer.py
@@ -1,12 +1,30 @@
 from unittest import TestCase
 
+import os
+
+from checkQC.run_type_recognizer import RunTypeRecognizer
 
 class TestRunTypeRecognizer(TestCase):
+
+    CONFIG = {"instrument_type_mappings":
+                  {"M": "miseq"}}
+
+    def setUp(self):
+        self.runtype_recognizer = RunTypeRecognizer(runfolder=os.path.join(os.path.dirname(__file__),
+                                                                           "resources", "MiSeqDemo"),
+                                                    config=self.CONFIG)
+
     def test_instrument_type(self):
-        self.fail()
+        expected = "miseq"
+        actual = self.runtype_recognizer.instrument_type()
+        self.assertEqual(expected, actual)
 
     def test_single_or_paired_end(self):
-        self.fail()
+        expected = "paired_end"
+        actual = self.runtype_recognizer.single_or_paired_end()
+        self.assertEqual(expected, actual)
 
     def test_read_length(self):
-        self.fail()
+        expected = "300-300"
+        actual = self.runtype_recognizer.read_length()
+        self.assertEqual(expected, actual)
