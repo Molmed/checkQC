f03b98309c33c83b88baabd5a30f1f84ed2b4732
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2017-08-08 16:27:03 +0200

    Add basic run type recog. test

---
+++ tests/test_run_type_recognizer.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class TestRunTypeRecognizer(TestCase):
+    def test_instrument_type(self):
+        self.fail()
+
+    def test_single_or_paired_end(self):
+        self.fail()
+
+    def test_read_length(self):
+        self.fail()
