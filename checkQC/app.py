d1a10f83f39f87fb6ee2facbd7308f0067e27be6
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2017-08-10 10:35:02 +0200

    Fix bugs introduced by refactoring

--- checkQC/app.py
+++ checkQC/app.py
@@ -21,7 +21,7 @@ def start(config_file, runfolder):
     # This is the application entry point
     # -----------------------------------
 
-    app = App(config_file, runfolder)
+    app = App(runfolder, config_file)
     app.run()
     sys.exit(app.exit_status)
 
@@ -34,26 +34,21 @@ class App(object):
         self.exit_status = 0
 
     def run(self):
-        try:
-            config = get_config(self._config_file)
+        config = get_config(self._config_file)
 
-            run_type_recognizer = RunTypeRecognizer(config=config, runfolder=self._runfolder)
-            instrument_type = run_type_recognizer.instrument_type()
-            reagent_version = run_type_recognizer.reagent_version()
+        run_type_recognizer = RunTypeRecognizer(config=config, runfolder=self._runfolder)
+        instrument_type = run_type_recognizer.instrument_type()
+        reagent_version = run_type_recognizer.reagent_version()
 
-            # TODO For now assume symmetric read lengths
-            read_length = int(run_type_recognizer.read_length().split("-")[0])
+        # TODO For now assume symmetric read lengths
+        read_length = int(run_type_recognizer.read_length().split("-")[0])
 
-            instrument_and_reagent_type = "_".join([instrument_type, reagent_version])
+        instrument_and_reagent_type = "_".join([instrument_type, reagent_version])
 
-            handler_config = get_handler_config(config, instrument_and_reagent_type, read_length)
-            qc_engine = QCEngine(runfolder=self._runfolder, handler_config=handler_config)
-            qc_engine.run()
-            self.exit_status = qc_engine.exit_status
-
-        except Exception as e:
-            print("Got an exception when running checkQC: {}".format(e))
-            self.exit_status = 1
+        handler_config = get_handler_config(config, instrument_and_reagent_type, read_length)
+        qc_engine = QCEngine(runfolder=self._runfolder, handler_config=handler_config)
+        qc_engine.run()
+        self.exit_status = qc_engine.exit_status
 
 if __name__ == '__main__':
     start()
--- tests/test_app.py
+++ tests/test_app.py
@@ -4,14 +4,16 @@ import os
 
 from checkQC.app import App
 
+
 class TestApp(unittest.TestCase):
 
     def setUp(self):
         self.app = App(runfolder=os.path.join(os.path.dirname(__file__), "resources", "MiSeqDemo"))
 
     def test_run(self):
+        # TODO Improve this test once we have better test data
         self.app.run()
-        self.assertEqual(self.app.exit_status, True)
+        self.assertEqual(self.app.exit_status, 1)
 
 
 if __name__ == '__main__':
