346974b5edef5a6928b714fc712657fe0619ad26
monikaBrandt <monika.brandt@medsci.uu.se>
2017-08-17 13:28:39 +0200

    Handler for Cluster pf implemented

--- checkQC/handlers/cluster_pf_handler.py
+++ checkQC/handlers/cluster_pf_handler.py
@@ -1,3 +1,5 @@
+from math import pow
+
 from checkQC.handlers.qc_handler import QCHandler, QCErrorFatal, QCErrorWarning
 from checkQC.parsers.stats_json_parser import StatsJsonParser
 
@@ -10,3 +12,21 @@ class ClusterPFHandler(QCHandler):
 
     def parser(self):
         return StatsJsonParser
+
+    def collect(self, signal):
+        key, value = signal
+        if key == "ConversionResults":
+            self.conversion_results = value
+
+    def check_qc(self):
+
+        for lane_dict in self.conversion_results:
+            lane_nbr = lane_dict["LaneNumber"]
+            lane_pf = lane_dict["TotalClustersPF"]
+
+            if self.qc_config["error"] != "unknown" and lane_pf < float(self.qc_config["error"])*pow(10, 6):
+                yield QCErrorFatal("Clusters PF was to low on lane {}, it was: {}".format(lane_nbr, lane_pf))
+            elif lane_pf < float(self.qc_config["warning"])*pow(10, 6):
+                yield QCErrorWarning("Yield was to low on lane {}, it was: {}".format(lane_nbr, lane_pf))
+            else:
+                continue
