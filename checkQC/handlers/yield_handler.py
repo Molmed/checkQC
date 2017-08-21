c106a7be6bfa48ecf2f58fd7bc54c621c4f404c1
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2017-08-21 10:42:04 +0200

    Removed raw reference to config error/warning

--- checkQC/handlers/yield_handler.py
+++ checkQC/handlers/yield_handler.py
@@ -25,9 +25,9 @@ class YieldHandler(QCHandler):
             lane_nbr = lane_dict["LaneNumber"]
             lane_yield = lane_dict["Yield"]
 
-            if self.error() != self.UNKNOWN and lane_yield < float(self.qc_config["error"]) * pow(10, 9):
+            if self.error() != self.UNKNOWN and lane_yield < float(self.error()) * pow(10, 9):
                 yield QCErrorFatal("Yield was to low on lane {}, it was: {}".format(lane_nbr, lane_yield))
-            elif self.warning() != self.UNKNOWN and lane_yield < float(self.qc_config["warning"]) * pow(10, 9):
+            elif self.warning() != self.UNKNOWN and lane_yield < float(self.warning()) * pow(10, 9):
                 yield QCErrorWarning("Yield was to low on lane {}, it was: {}".format(lane_nbr, lane_yield))
             else:
                 continue
