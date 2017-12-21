151bc8a498fe0cdff15432bb285f30c637c6a0e9
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2017-12-21 14:54:10 +0100

    Improving API docs in qc_handler module

--- checkQC/handlers/qc_handler.py
+++ checkQC/handlers/qc_handler.py
@@ -6,36 +6,43 @@ from checkQC.config import ConfigurationError
 log = logging.getLogger()
 
 
-class Subscriber(object):
-    def __init__(self):
-        self.subscriber = self.subscribe()
-        next(self.subscriber)
-
-    def subscribe(self):
-        while True:
-            value = yield
-            self.collect(value)
-
-    def collect(self):
-        raise NotImplementedError()
-
-    def send(self, value):
-        self.subscriber.send(value)
-
-
 class QCHandlerReport(object):
+    """
+    Base class of objects which contain reports from a QCHandler
+    """
+
     def __init__(self, msg, ordering=1, data=None):
+        """
+        Instantiate a new QCHandlerReport
+        :param msg: Message to send
+        :param ordering: A number indicating the ordering of this object in relation to other objects of the same type
+                        can be used e.g. to order reports by lane
+        :param data: Any additional data (this can e.g. be used to dump additional data into a json object), should be
+                     a dict
+        """
         self.message = msg
         self.ordering = ordering
         self.data = data
 
     def type(self):
+        """
+        Should be implemented by the subclass.
+        :return: String with the type of the report, e.g. "error" or "waring"
+        """
         raise NotImplementedError("Subclass must implement this method")
 
     def __repr__(self):
+        """
+        Providing string representation of this class
+        :return: Class string representation
+        """
         return self.__str__()
 
     def as_dict(self):
+        """
+        Dump the class as a dictionary
+        :return: A dict of representing this QCHandlerReport
+        """
         return {'type': self.type(), 'message': self.message, 'data': self.data}
 
 
@@ -59,6 +66,54 @@ class QCErrorWarning(QCHandlerReport):
 
     def type(self):
         return "warning"
+
+
+class Subscriber(object):
+    """
+    Subscriber defined the behaviour necessary to subscribe to data from a Parser. The implementing subclass
+    has to implement the `collect` method. This method can decide which objects sent to the Subscriber that
+    are of interest to that particular Subscriber, and what should be done with those values.
+    """
+
+    def __init__(self):
+        self.subscriber = self.subscribe()
+        next(self.subscriber)
+
+    def subscribe(self):
+        """
+        This method picks up data from the parser to which the Subscriber is listening.
+        :return:
+        """
+        while True:
+            value = yield
+            self.collect(value)
+
+    def collect(self, signal):
+        """
+        The implementing subclass should provide this method. It is up to instance receiving the data to decide
+        how to handle it. Below is an example of how to handle a tuple with a key-value pair.
+        ```
+        class MySubscriber(Subscriber):
+
+            def __init__(self):
+                self.results = []
+
+            def collect(self, signal):
+                key, value = signal
+                if key == "my_key":
+                    self.results.append(value)
+        ```
+        :return: None
+        """
+        raise NotImplementedError("Implementing class must provide this method.")
+
+    def send(self, value):
+        """
+        Will send the specified value to the subscriber
+        :param value: Value to send to subscriber
+        :return: None
+        """
+        self.subscriber.send(value)
 
 
 class QCHandler(Subscriber):
@@ -98,9 +153,6 @@ class QCHandler(Subscriber):
 
     def parser(self):
         raise NotImplementedError("A handler needs to return the class of the parser it needs!")
-
-    def collect(self, value):
-        raise NotImplementedError
 
     def check_qc(self):
         raise NotImplementedError
