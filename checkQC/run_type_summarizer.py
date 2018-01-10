524793d7904fa85b7e3e6b61711d2cc80be46f40
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2018-01-10 15:34:56 +0100

    API Docs for RunTypeSummarizer

--- checkQC/run_type_summarizer.py
+++ checkQC/run_type_summarizer.py
@@ -5,9 +5,22 @@ log = logging.getLogger(__name__)
 
 
 class RunTypeSummarizer(object):
+    """
+    RunTypeSummarizer contains methods used to summarize information about a run (mostly in order
+    to be able to report this back to the user in one for or another).
+    """
 
     @staticmethod
     def summarize(instrument_and_reagent_version, read_lengths, handler_config):
+        """
+        Utility method to parse log and summarize the information about a particular run.
+        This is used to tell the user what information CheckQC has gathered about a run, and
+        what configuration was used.
+        :param instrument_and_reagent_version: str with instrument and reagent version
+        :param read_lengths: str with read length
+        :param handler_config: dict with handler configuration
+        :return: dict with information on the handlers used
+        """
 
         summary = {}
 
