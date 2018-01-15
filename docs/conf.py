195c8d68d5b4fa5cab967bcdfeb481f30ce6c29e
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2018-01-15 12:21:58 +0100

    Trying to get ReadTheDocs working

--- docs/conf.py
+++ docs/conf.py
@@ -179,3 +179,16 @@ source_parsers = {
 source_suffix = ['.rst', '.md']
 
 
+def run_apidoc(_):
+    from sphinx.apidoc import main
+    import os
+    import sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+    cur_dir = os.path.abspath(os.path.dirname(__file__))
+    module = os.path.join(cur_dir,"..","checkQC")
+    main(['-e', '-o', cur_dir, module, '--force'])
+
+def setup(app):
+    app.connect('builder-inited', run_apidoc)
+
+
