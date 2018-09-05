f652389e00942a2b9f7e5c22c40b7939e1014322
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2018-09-05 08:50:02 +0200

    Improved docs for RunfolderReader

--- checkQC/runfolder_reader.py
+++ checkQC/runfolder_reader.py
@@ -8,9 +8,21 @@ log = logging.getLogger(__name__)
 
 
 class RunfolderReader(object):
+    """
+    This class provides methods to read files such as the runParameters.xml and RunInfo.xml files, which
+    often need to be read to pick up info about what type of run we are looking at etc.
+    """
 
     @staticmethod
     def read_run_parameters_xml(runfolder):
+        """
+        Read the run parameters of an Illumina instrument are recorded in a file called
+        runParameters or RunParameters depending on the exact instrument type. This method
+        will read it and return it as a dict.
+        :param runfolder: to look in
+        :return: the [R|r]unParameters.xml as a dict
+        :raises: RunParametersNotFound if no [R|r]unParameters.xml was found
+        """
         try:
             with open(RunfolderReader.find_run_parameters_xml(runfolder)) as f:
                 return xmltodict.parse(f.read())
@@ -19,6 +31,11 @@ class RunfolderReader(object):
 
     @staticmethod
     def read_run_info_xml(runfolder):
+        """
+        Read the RunInfo.xml and return it as a dict
+        :param runfolder: to look in
+        :return: RunInfo.xml data as a dict
+        """
         try:
             run_info_path = os.path.join(runfolder, "RunInfo.xml")
             if not os.path.exists(run_info_path):
@@ -31,6 +48,11 @@ class RunfolderReader(object):
 
     @staticmethod
     def find_run_parameters_xml(runfolder):
+        """
+        Finds the path to the [r|R]unParameters.xml
+        :param runfolder: to look in
+        :return: the path to the [r|R]unParameters.xml
+        """
         first_option = os.path.join(runfolder, "RunParameters.xml")
         second_option = os.path.join(runfolder, "runParameters.xml")
         if os.path.isfile(first_option):
