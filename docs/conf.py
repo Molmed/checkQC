1c7a0736fbbdfd0d99ea9578aca114eeb84b6b77
Adrien Coulier <adrien.coulier@medsci.uu.se>
2023-05-09 16:06:24 +0200

    Develop 2548 add nova seq x support (#106)
    * Make instrument recognizer case-insensitive
    
    * Update dependencies
    
    * Add mock again to the requirements
    
    This was removed by mistake
    
    * Update markdown parsing for Sphinx
    
    Atm `myst-parser` does not support the last version of sphinx and
    requires `sphinx<7`.
    
    * Fix a couple warnings when fixing sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -22,9 +22,6 @@ import sys
 sys.path.insert(0, os.path.abspath('../checkQC'))
 
 
-from recommonmark.parser import CommonMarkParser
-
-
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -34,8 +31,11 @@ from recommonmark.parser import CommonMarkParser
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.viewcode']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'myst_parser',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -68,7 +68,7 @@ release = ''
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -171,10 +171,6 @@ texinfo_documents = [
      'Miscellaneous'),
 ]
 
-
-source_parsers = {
-            '.md': CommonMarkParser,
-            }
 
 source_suffix = ['.rst', '.md']
 
