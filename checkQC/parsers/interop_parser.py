782667947877aeb4fcf53dec28df328a4fa770e6
Monika Brandt <monika.brandt@medsci.uu.se>
2024-06-03 14:59:28 +0200

    Update interop parser to get Q30 per cycle
    This commit will contribute with the following features:
    - A new function, imaging, is imported from the interop library.
    - Imaging is used in the run-function to read the interop "Q"-table
      where detailed %Q30 metrics can be found (not only summary per lane
    and read as in the summary-table).
    - A new function get_percent_q30_per_cycle is implemented to summaries
    %Q30 metrics over cycles to be able to detect specific quality drops in
    specific cycles.
    - A unittest is implemented to test the new function.

--- checkQC/parsers/interop_parser.py
+++ checkQC/parsers/interop_parser.py
@@ -4,6 +4,9 @@ from checkQC.parsers.parser import Parser
 from interop import py_interop_run_metrics, py_interop_run, \
     py_interop_summary, imaging
 
+import pandas as pd
+import math
+import numpy
 
 
 class InteropParser(Parser):
@@ -57,17 +60,62 @@ class InteropParser(Parser):
                 non_index_reads.append(read_nbr)
         return non_index_reads
     
-    def get_percent_q30_per_cycle():
+    @staticmethod
+    def get_percent_q30_per_cycle(q_metrics, lane_nr, read_nr, is_index_read):
         """
         For each lane, read,swath and tile get the mean percent_q30
         for each cycle. Only include the first 90% of the cycles, i.e.
-        for a run with 151 cycles, only use the first 136 cycles.
-
+        for a run with 151 cycles, only use the first 136 cycles. 
+        Also remove the first 5 cycles since there might be a quality drop there.
+        #TODO: Investigate if the quality drop in the beginning of a read
+        is instrument specific. 
+        :param q_metrics: A rectangular array containing the following
+        rec.array([(1., 1101.,   1., 1.,   1., 99.73, 98.3 , 1., 1.,  1.),
+           (1., 1101.,   3., 1.,   3., 99.6 , 98.37, 1., 1.,  1.), ...,
+           (1., 2119., 602., 2., 301., 38.07, 13.91, 2., 1., 19.)],
+           dtype=[('Lane', '<f4'), ('Tile', '<f4'), ('Cycle', '<f4'), 
+           ('Read', '<f4'), ('Cycle Within Read', '<f4'), 
+           ('%>= Q20', '<f4'), ('%>= Q30', '<f4'), ('Surface', '<f4'), 
+           ('Swath', '<f4'), ('Tile Number', '<f4')])
+        :param lane: 0-indexed lane, integer 0-7
+        :param read: 0-indexed read, integer 0-3
         :returns: A list where each element is the mean percent_q30 
         for the cycle corresponding to the position in the list, i.e
-        list[0] = cycle 1 
+        list[0] = cycle 1
+        #TODO: Return a dictionary instead of a list. Use "Cycle within read"
+        as the key and mean %Q30 as the value. This is to keep track of 
+        the cycle since it does no longer correspond to position in list.  
         """
-        return []
+        q30_cycle_df = pd.DataFrame(q_metrics)
+        q30_lane_read = q30_cycle_df[
+                (q30_cycle_df["Lane"] == lane_nr+1) &\
+                (q30_cycle_df["Read"] == read_nr+1)]
+        
+        q30_per_cycle = []
+        if is_index_read:
+            cycles_q_metrics = int(max(q30_lane_read["Cycle Within Read"]))
+            start_cycle = cycles_q_metrics - 5
+            #Remove the first 2 bases in the index read.
+
+            #TODO: How to handle for example 6bp reads in a 8bp run?
+            #The first 2 bases will have a quality of 0 in our test run.
+            #Is this the case for all runs?
+            #For now I will solve it by only looking at the last 6 bp.
+
+        else:
+            cycles_q_metrics = math.ceil(max(q30_lane_read["Cycle Within Read"])*0.9)
+            #Remove the last 90% of all cycles since they are expected to drop in q30
+            start_cycle = 6
+            #Remove first 5 cycles since they are expected to have a lower q30
+
+        for cycle in range(start_cycle,cycles_q_metrics+1):
+                
+            q30_cycle_mean = numpy.mean(q30_lane_read[
+                (q30_lane_read["Cycle Within Read"] == cycle)]["%>= Q30"])
+            q30_per_cycle.append(q30_cycle_mean)
+
+        return q30_per_cycle
+
 
     def run(self):
         run_metrics = py_interop_run_metrics.run_metrics()
@@ -76,6 +124,8 @@ class InteropParser(Parser):
         valid_to_load = py_interop_run.uchar_vector(py_interop_run.MetricCount, 0)
         py_interop_run_metrics.list_summary_metrics_to_load(valid_to_load)
         run_metrics.read(self.runfolder, valid_to_load)
+        
+        q_metrics = imaging(self.runfolder, valid_to_load=['Q'])
 
         summary = py_interop_summary.run_summary()
         py_interop_summary.summarize_run_metrics(run_metrics, summary)
@@ -92,7 +142,11 @@ class InteropParser(Parser):
                 percent_q30 = read.percent_gt_q30()
                 percent_phix_aligned = read.percent_aligned().mean()
                 is_index_read = summary.at(read_nbr).read().is_index()
-                percent_q30_per_cycle = []
+                percent_q30_per_cycle = self.get_percent_q30_per_cycle(
+                                        q_metrics,
+                                        lane,
+                                        read_nbr,
+                                        is_index_read)
                 
                 self._send_to_subscribers(("error_rate",
                                         {"lane": lane+1, "read": read_nbr+1, "error_rate": error_rate}))
